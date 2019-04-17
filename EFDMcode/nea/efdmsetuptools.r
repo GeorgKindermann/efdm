8ab166c54db3e41799d6ed6e8f36e39590b1a7ee
GeorgKindermann <georg.kindermann@bfw.gv.at>
2019-04-17 12:26:27 +0200

    Removed bug in EFDM-nea pre.estimate concerning names of transition factors

--- EFDMcode/nea/efdmsetuptools.r
+++ EFDMcode/nea/efdmsetuptools.r
@@ -66,7 +66,8 @@ pre.estimate<-function(inputfilename,factorsfilename,changing,resultfilename)
   #(with the needed data and possible prior available of course)
 {
   setupstatespace(factorsfilename)
-  basedimnames<<-strsplit("stem vol",split=" ")[[1]]
+  #basedimnames<<-strsplit("stem vol",split=" ")[[1]] #gk
+  basedimnames <<- strsplit(changing, split=" ")[[1]] #gk
   otherdimnames<<-setdiff(factnames,basedimnames)
   basedim<<-prod(factdims[basedimnames])
   otherdim<<-factdims[otherdimnames]
